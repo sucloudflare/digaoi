cd3ff6175d4111586eb941797591510672ec55c0
Bruno <135662753+sucloudflare@users.noreply.github.com>
2025-09-24 16:55:44 -0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2025 Bruno
+ * Todos os direitos reservados.
+ * Criado em: 24/09/2025
+ */
+
+// Código de exemplo
+console.log("Olá, este é um arquivo de teste com copyright!");
